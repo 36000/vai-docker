b117909ab875ca1a4e3322a7e8cf7faf60a57e4e
36000 <jk6.28@outlook.com>
2025-07-17 00:35:11 -0700

    bf

--- run_yellow_earth.sh
+++ run_yellow_earth.sh
@@ -1,8 +1,8 @@
 srun\
-    --time=4:00:00\
-    -p gpu-rtx6k\
+    --time=24:00:00\
+    -p gpu-a40\
     -A escience\
     --cpus-per-task=4\
-    --gpus=1\
+    --gpus=2\
     --mem=32G\
     make yellowearth
